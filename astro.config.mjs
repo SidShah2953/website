df097021dc70bc60a60aa5e6bb359483f8caa2db
SidShah2953 <sidshah2953@gmail.com>
2023-10-20 16:54:50 +0530

    Files for Github Pages - added base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import { SITE_URL } from "./src/data/config";
+import { SITE_URL } from "@/data/config.js";
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
+  base: '/SidShah2953',
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
