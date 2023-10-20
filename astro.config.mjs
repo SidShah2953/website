82b276b6fa11339176021b9f289b80c818981908
SidShah2953 <sidshah2953@gmail.com>
2023-10-20 17:04:13 +0530

    Files for Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "@/data/config.js";
+import netlify from '@astrojs/netlify/functions';
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
-  base: '/SidShah2953',
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
