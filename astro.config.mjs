5607a96c76cfd9749c0ca37bc04df73b8ec87d25
SidShah2953 <sidshah2953@gmail.com>
2023-10-20 17:18:40 +0530

    Made website static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import { SITE_URL } from "@/data/config.js";
-import netlify from '@astrojs/netlify/functions';
+import { SITE_URL } from "./src/data/config";
 
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
   markdown: {
