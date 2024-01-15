6db43b9b92bce2508f99c885ffe95acaba3b4706
SidShah2953 <sidshah2953@gmail.com>
2024-01-15 17:10:38 +0530

    - Added a Projects Page & Collecttion - Changed some CSS

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,8 +7,19 @@ const postsCollection = defineCollection({
     publishedAt: z.date(),
     description: z.string(),
     isPublish: z.boolean(),
-    isDraft: z.boolean().default(false),
+    isDraft: z.boolean().default(false)
   }),
 });
 
-export const collections = { posts: postsCollection };
+const projectsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    publishedAt: z.date(),
+    description: z.string(),
+    isPublish: z.boolean(),
+    isOngoing: z.boolean().default(false)
+  }),
+});
+
+export const collections = { posts: postsCollection, projects: projectsCollection};
