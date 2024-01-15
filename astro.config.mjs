cddb8badc1dff55b1b08f4c30a9ba8d261fd9176
SidShah2953 <sidshah2953@gmail.com>
2024-01-15 16:11:37 +0530

    - Upgraded Astro - Added MDX Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,16 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
 
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), robotsTxt(), mdx()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
-      theme: "nord",
+      theme: "dracula",
       wrap: false
     }
   }
