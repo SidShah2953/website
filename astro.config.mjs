51fd46d9795f7a85a45d7117705ae065cf12c149
SidShah2953 <sidshah2953@gmail.com>
2024-01-15 16:25:35 +0530

    - Added Latex Configs (refer to https://r3zz.io/posts/astro-blog-latex/#objective) - Changed md to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,19 @@ import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
 
 import mdx from "@astrojs/mdx";
+// For Latex Integration 
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt(), mdx()],
+  integrations: [tailwind(),
+    sitemap(),
+    robotsTxt(),
+    mdx({
+			remarkPlugins: [remarkMath], // For Latex Integration
+			rehypePlugins: [rehypeKatex] // For Latex Integration
+		})],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
