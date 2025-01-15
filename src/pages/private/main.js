40d80a7bd6f0fb0e8bcb6a92df5500b2289a6446
SidShah2953 <sidshah2953@gmail.com>
2025-01-14 23:51:02 -0600

    Removed HTML and JS

--- src/pages/private/main.js
+++
@@ -1,7 +0,0 @@
-onload = () => {
-    const c = setTimeout(() => {
-      const div = document.getElementById('myDiv');
-      div.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
